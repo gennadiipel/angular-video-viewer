4e33b03cf916b3f6241a2eaa9b1f1f11584b6c53
pelhenry <thedankertanker@outlook.com>
2021-03-13 18:30:58 +0200

    started control component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { VideoContainerComponent } from './components/video-container/video-container.component';
 import { VideoToolbarComponent } from './components/video-toolbar/video-toolbar.component';
+import { PlayPauseButtonComponent } from './components/controls/play-pause-button/play-pause-button.component';
+import { GoogleIconModule } from 'google-icon';
+import { GoogleIconComponent } from 'google-icon/google-icon/google-icon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VideoContainerComponent,
-    VideoToolbarComponent
+    VideoToolbarComponent,
+    PlayPauseButtonComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
