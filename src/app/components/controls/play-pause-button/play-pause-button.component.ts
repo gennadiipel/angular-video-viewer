602961fd2e4589b19d53e41bb1f5c940ce2f4f92
pelhenry <thedankertanker@outlook.com>
2021-03-14 11:14:30 +0200

    play pause control

--- src/app/components/controls/play-pause-button/play-pause-button.component.ts
+++ src/app/components/controls/play-pause-button/play-pause-button.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayPauseButtonComponent implements OnInit {
 
+  isPlaying: boolean = true
+  currentIcon: string = '/assets/play_arrow.svg'
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggle() {
+    this.isPlaying = !this.isPlaying
+    this.currentIcon = this.isPlaying ? '/assets/play_arrow.svg' : '/assets/pause.svg'
   }
 
 }
